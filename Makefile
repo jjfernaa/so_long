c4e6ad0e576c18d4fee8c04bf865d820a976d07d
Juan Fernández <juanjose.fernanfez88@gmail.com>
2025-05-10 03:28:51 +0200

    init

---
+++ Makefile
@@ -0,0 +1,50 @@
+NAME = so_long
+
+CC = CC
+CFLAGS = -Wall -Wextra -Werror
+
+SRC_DIR = srcs
+OBJ_DIR = obj
+INC_DIR = includes
+LIBFT_DIR = libft
+MLX42_DIR = MLX42
+
+SRC_FILES = main.c init.c map_parser.c map_checker.c render.c player.c utils.c error.c
+SRCS = $(addprefix $(SRC_DIR)/, $(SRC_FILES))
+OBJS = $(addprefix $(OBJ_DIR)/, $(SRC_FILES:.c=.o))
+
+LIBFT = $(LIBFT_DIR)/libft.a
+MLX42 = $(MLX42_DIR)/bluid/libmlx42.a
+
+DEPS = -lglfw -L "/opt/homebrew/Cellar/glfw/3.3.8/lib/" -framework Cocoa -framework OpenGL -framework IOKit
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(MLX42) $(OBJ_DIR) $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME) $(LIBFT) $(MLX42) $(DEPS) -I $(INC_DIR) -I $(LIBFT_DIR) -I $(MLX42_DIR)/includes
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	$(CC) $(CFLAGS) -c $< -o $@ -I $(INC_DIR) -I $(LIBFT_DIR) -I $(MLX42_DIR)/includes
+
+$(LIBFT):
+	make -C $(LIBFT_DIR)
+
+$(MLX42)
+	cmake -B $(MLX42_DIR)/bluid $(MLX42_DIR)
+	cmake --bluid $(MLX42_DIR)/bluid -j4
+
+Clean:
+	rm -rf $(OBJ_DIR)
+	make -C $(LIBFT_DIR) clean
+	rm -rf $(MLX42_DIR)/bluid
+
+fclean:
+	rm -f $(NAME)
+	make -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/so_long.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juan-jof <juan-jof@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 00:43:40 by juan-jof          #+#    #+#             */
+/*   Updated: 2025/05/10 03:16:22 by juan-jof         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../Libft/libft.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "fcntl.h"
+# include "stdlib.h"
+# include "stdio.h"
+
+# define EMPTY '0'
+# define WALL '1'
+# define COLLECTIBLE 'C'
+# define EXIT 'E'
+# define PLAYER 'P'
+
+#define TILE_SIZE 32
+
+# define KEY_w 87
+# define KEY_A 65
+# define KEY_S 83
+# define KEY_D 68
+# define KEY_ESC 256
+
+
+#endif
